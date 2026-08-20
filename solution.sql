6125e873cb04db7688909f77dbb949a0fc645d1f
SARAN KARTHIK.M <sarankarthik399@gmail.com>
2026-08-20 13:44:20 +0530

    PROGRAM 1 RDBMS 1U25IT092

--- solution.sql
+++ solution.sql
@@ -2,7 +2,7 @@ CREATE DATABASE COllegeDB;
 USE CollegeDB;
 CREATE TABLE Department (
   DepartmentID NUMBER(5) PRIMARY KEY,
-  DepartmentName VARCHAR(20) NOT NULL,
-  HOD VARCHAR(20) NOT NULL
+  DepartmentName VARCHAR(20),
+  HOD VARCHAR(20)
   );
-DESC Department;
+DESC Department
